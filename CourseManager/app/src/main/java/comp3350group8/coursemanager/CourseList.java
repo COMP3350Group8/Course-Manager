346a967ac6464f778d785bfd78a8df431dab5b63
Ian Smith <ian_smith_12a@mymts.net>
2016-02-06 09:56:46 -0600

    Init commit

---
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/CourseList.java
@@ -0,0 +1,13 @@
+package comp3350group8.coursemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list);
+    }
+}
