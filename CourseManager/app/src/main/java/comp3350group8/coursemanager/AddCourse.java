9408e0638009a912f56f73c8d17e343255712ea4
Ian Smith <ian_smith_12a@mymts.net>
2016-02-07 21:26:03 -0600

    Added some simple UI for adding more courses

---
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/AddCourse.java
@@ -0,0 +1,13 @@
+package comp3350group8.coursemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+    }
+}
