e4319f4a835a4df10b8f0240d43138150e79c39d
Ian Smith <ian_smith_12a@mymts.net>
2016-02-17 19:23:56 -0600

    Implements the AppDatabase interface in the most basic way possible

---
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/SQLDatabase.java
@@ -0,0 +1,11 @@
+package comp3350group8.coursemanager;
+
+/**
+ * Created by Ian Smith on 2016-02-17.
+ */
+public class SQLDatabase implements AppDatabase {
+    public void insert(ListItem item) {}
+    public void update(int id, ListItem item) {}
+    public ListItem remove(int id) {return null;}
+    public String[] read() {return null;}
+}
