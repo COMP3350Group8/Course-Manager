1ecf751f1a75f14d3bdc7f632a833b7d8efe1e0f
Ian Smith <ian_smith_12a@mymts.net>
2016-02-25 22:32:21 -0600

    SQL class no longer implements any interface

--- CourseManager/app/src/main/java/comp3350group8/coursemanager/SQLDatabase.java
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/SQLDatabase.java
@@ -3,7 +3,7 @@ package comp3350group8.coursemanager;
 /**
  * Created by Ian Smith on 2016-02-17.
  */
-public class SQLDatabase implements AppDatabase {
+public class SQLDatabase {
     public void insert(ListItem item) {}
     public void update(int id, ListItem item) {}
     public ListItem remove(int id) {return null;}
