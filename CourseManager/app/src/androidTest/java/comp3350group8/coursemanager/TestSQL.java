d96fbd485cceb735beaf2100f869bb6f7689c621
Ian Smith <ian_smith_12a@mymts.net>
2016-03-02 19:08:56 -0600

    Created a test for the SQLite database

---
+++ CourseManager/app/src/androidTest/java/comp3350group8/coursemanager/TestSQL.java
@@ -0,0 +1,12 @@
+package comp3350group8.coursemanager;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Ian Smith on 2016-03-02.
+ */
+public class TestSQL extends TestCase {
+    public void testCase() {
+
+    }
+}
