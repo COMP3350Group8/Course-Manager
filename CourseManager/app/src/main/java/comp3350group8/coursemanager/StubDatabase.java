d28c3e798efd41288058f33f96de05d3eee286b2
Ian Smith <ian_smith_12a@mymts.net>
2016-03-11 17:32:50 -0600

    Did a dependency injection on the stub, it is now a subclass of the SQLDatabase class

--- CourseManager/app/src/main/java/comp3350group8/coursemanager/StubDatabase.java
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/StubDatabase.java
@@ -1,5 +1,8 @@
 package comp3350group8.coursemanager;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 import java.util.ArrayList;
 
 import comp3350group8.coursemanager.AppDatabase;
@@ -8,33 +11,74 @@ import comp3350group8.coursemanager.ListItem;
 /**
  * Created by Ian Smith on 2016-02-12.
  */
-public class StubDatabase implements AppDatabase {
-    private ArrayList<ListItem> list;
 
-    public StubDatabase() {
-        list = new ArrayList<ListItem>();
+import java.util.ArrayList;
+
+/**
+ * Created by David on 2016-02-22.
+ */
+public class StubDatabase extends SQLDatabase {
+    private static ArrayList<SubTable> db;
+
+    public StubDatabase(Context context) {
+        super(context);
+        db = new ArrayList<SubTable>();
+        db.add(new SubTable("Courses"));
     }
 
-    public void insert(ListItem item) {
-        list.add(item);
-    }
+    //@Override
+    //public void onCreate(SQLiteDatabase data) {
+        //db = new ArrayList<SubTable>();
+        //db.add(new SubTable("Courses"));
+    //}
 
-    public void update(int id, ListItem item) {
-        ListItem o = list.get(id);
-        list.add(id, item);
-    }
+    private static final String TABLE_COURSES = "Courses";
+    private static final String[] COURSE_COLUMNS = {"ID", "CourseName", "CourseLocation", "CourseDescription"};
 
-    public ListItem remove(int id) {
-        return list.remove(id);
-    }
+    private SubTable retrieveTable(String name) {
+        SubTable out = null;
 
-    public String[] read() {
-        String[] array = new String[list.size()];
-
-        for (int i = 0; i < list.size(); i++) {
-            array[i] = list.get(i).toString();
+        for (int i = 0; i < db.size() && out == null; i++) {
+            if (db.get(i).getName().equals(name)) {
+                out = db.get(i);
+            }
         }
 
-        return array;
+        return out;
     }
+
+    public void insertCourse(Course course) {
+        SubTable table = retrieveTable("Courses");
+        table.insert(course);
+    }
+
+    public Course getCourse(int id) {
+        Course course = null;
+
+        if (id > 0) {
+            SubTable table = retrieveTable("Courses");
+
+            if (id < table.size()) {
+                course = (Course)table.get(id);
+            }
+        }
+
+        return course;
+    }
+
+    public ArrayList<Course> getAllCourses() {
+        ArrayList<ListItem> list = retrieveTable("Courses").getAll();
+
+        ArrayList<Course> out = new ArrayList<Course>();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof Course) {
+                Course course = (Course) list.get(i);
+                out.add(course);
+            }
+        }
+
+        return out;
+    }
+
 }
