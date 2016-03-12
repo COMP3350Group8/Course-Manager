a7380b54ebfe9ee8e6256c6575c9f57a70efc003
davidowasi <owasidavid@gmail.com>
2016-03-12 11:15:31 -0600

    Created a class to store user information, captured user information from main screen

--- CourseManager/app/src/main/java/comp3350group8/coursemanager/MainScreen.java
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/MainScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import comp3350group8.coursemanager.R;
 
@@ -20,15 +21,21 @@ public class MainScreen extends AppCompatActivity {
 
     public void buttonOnClick (View v)
     {
+        EditText name, password, studentNum, email, school;
+        name = (EditText) findViewById(R.id.name);
+        password= (EditText) findViewById(R.id.password);
+        studentNum = (EditText) findViewById(R.id.studentNum);
+        email = (EditText) findViewById(R.id.emailAddress);
+        school = (EditText) findViewById(R.id.school);
 
+        User newUser = new User(name.toString(),password.toString(), studentNum.toString(),
+                email.toString(),school.toString());
         startActivity(new Intent(MainScreen.this, ListOfCourses.class));
-
     }
 
     public void buttonOnClick2 (View v)
     {
-
-        //startActivity(new Intent(MainScreen.this, LoginActivity.class));
+        startActivity(new Intent(MainScreen.this, LoginActivity.class));
 
     }
 }
---
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/User.java
@@ -0,0 +1,32 @@
+package comp3350group8.coursemanager;
+
+/**
+ * Created by David on 2016-03-12.
+ */
+public class User {
+    private String name, password, studentNum, email, school;
+
+    public User(String name, String password, String studentNum, String email, String school)
+    {
+        this.name= name;
+        this.password= password;
+        this.studentNum = studentNum;
+        this.email= email;
+        this.school = school;
+    }
+
+    public String getPassWord()
+    {
+        return password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
