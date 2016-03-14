0d0dd94a3db8e7e7b97d1f4557bb8c151ed33232
umyee9 <anthony.yee0@gmail.com>
2016-03-14 00:39:02 -0500

    Task List and Add Task class added

---
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/Presentation/AddTask.java
@@ -0,0 +1,56 @@
+package comp3350group8.coursemanager;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import comp3350group8.coursemanager.R;
+
+/**
+ * Created by Anthony on 2016-03-08.
+ */
+public class AddTask extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addtask);
+
+
+    }
+
+    //Back Button
+    public void button5OnClick (View v)
+    {
+        startActivity(new Intent(AddTask.this, TaskList.class));
+
+    }
+
+    //Save Task Button
+    public void button6OnClick (View v)
+    {
+        // String []data = new String[3];
+
+        // retrieve the data supplied to the form
+        EditText taskName = (EditText) findViewById(R.id.taskName);
+        if(taskNameName.length() ==  0) {
+            taskNameName.setError("Enter task");
+        }
+        else {
+            EditText taskduedate = (EditText) findViewById(R.id.taskDueDate);
+
+            EditText taskduetime = (EditText) findViewById(R.id.taskDueTime);
+
+            // create instance of Task and send to the database
+            Task newTask = new Task(taskName.getText().toString(),
+                    taskduedate.getText().toString(),
+                    taskduetime.getText().toString());
+            staticDB.insert(newTask, "Tasks");
+
+            //data[0]= taskName.getText().toString();
+            //data[1] = taskduedate.getText().toString();
+            //data[2] = taskduetime.getText().toString();
+            startActivity(new Intent(AddTask.this, TaskList.class));
+
+        }
+    }
