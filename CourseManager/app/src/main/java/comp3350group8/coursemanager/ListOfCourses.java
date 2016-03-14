fcbcf1fa1ee9e11add3152a4f3a772a796193b04
sholajay <sholajayusuf@ymail.com>
2016-03-13 21:04:01 -0500

    click on course item to view course info

--- CourseManager/app/src/main/java/comp3350group8/coursemanager/ListOfCourses.java
+++ CourseManager/app/src/main/java/comp3350group8/coursemanager/ListOfCourses.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import comp3350group8.coursemanager.R;
 
@@ -32,6 +34,15 @@ public class ListOfCourses extends Activity {
         String[] course = staticDB.getTable("Courses");
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, course);
         lv.setAdapter(adapter);
+
+        //select a course
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Object o = lv.getItemAtPosition(position);
+                Toast.makeText(ListOfCourses.this, o.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
     public void buttonOnClick (View v)
     {
